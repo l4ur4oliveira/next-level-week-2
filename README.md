0f15b5a629fd2c31448d420067c003daced10b93
Laura de Oliveira <l4ur4.88@gmail.com>
2020-08-08 16:51:16 -0300

    Aula 5 - Inclusão de banco de dados, cadastro e busca de proffys

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
-# Next Level Week #2
+# Next Level Week #2 :rocket:
 
-Projeto desenvolvido durante o [curso](https://nextlevelweek.com/) com as principais noções de HTML, CSS e Javscript.
+Projeto desenvolvido durante a trilha **[Discovery](https://nextlevelweek.com/)** com as principais noções de HTML5, CSS3 e Javscript.
 
-- [Roadmap Front-end](https://www.notion.so/Front-end-ab15ef64dbe7459aba38364cf60af9d2)
-- [Roadmap Back-end](https://www.notion.so/Back-end-4440c9aeda8c47d4856a8e4d4069e379)
+## Init
+
+Instale as dependências com `npm install` e rode o servidor usando `npm run dev`.
+
+Se tudo estiver ok com a instalação, a aplicação poderá ser acessada em `localhost:5500`.
+
+## Roadmaps
+
+- [Front-end](https://www.notion.so/Front-end-ab15ef64dbe7459aba38364cf60af9d2)
+- [Back-end](https://www.notion.so/Back-end-4440c9aeda8c47d4856a8e4d4069e379)
+- [Database](https://www.notion.so/Banco-de-Dados-c6b7589f7ca740979a746d9289ab71f6)
 
 ## Layout
 
@@ -15,24 +24,34 @@ Projeto desenvolvido durante o [curso](https://nextlevelweek.com/) com as princi
 - Figma
 - Visula Studio Code
     - [Live Server Extension](https://github.com/ritwickdey/vscode-live-server)
+    - [SQLite Extension](https://github.com/AlexCovizzi/vscode-sqlite)
 - Google Chrome
 
-## Project
+## Project files structure
+
 ```
 /public
-    /images
-    /scripts
-        addField.js
-    /styles
-        main.css
-        ../partials
-            forms.css
-            header.css
-            page-landing.css
-            page-study.css
-            page-give-classes.css
+   /images
+   /scripts
+      ..addField.js
+   /styles
+     ..main.css
+     /partials
+        ..forms.css
+        ..header.css
+        ..page-landing.css
+        ..page-study.css
+        ..page-give-classes.css
 /src
-    index.html
-    study.html
-    give-classes.html
+   ..pages.js
+   ..server.js
+   /db
+      ..createProffy.js
+      ..db.js
+   /utils
+      ..format.js
+   /views
+      ..index.html
+      ..study.html
+      ..give-classes.html
 ```
--- src/server.js
+++ src/server.js
@@ -1,89 +1,12 @@
-// DADOS
-const proffys = [
-    {
-        name: "Beyoncé Knowles-Carter",
-        avatar: "https://avataaars.io/?avatarStyle=Circle&topType=LongHairCurvy&accessoriesType=Kurt&hairColor=BrownDark&facialHairType=Blank&clotheType=Overall&clotheColor=PastelOrange&eyeType=Close&eyebrowType=UpDown&mouthType=Twinkle&skinColor=DarkBrown",
-        whatsapp: "5551988776655",
-        bio: "Beyoncé Giselle Knowles-Carter é uma cantora, compositora e atriz norte-americana. Nascida e criada em Houston, no Texas, Beyoncé se tornou conhecida no ano de 1997, como uma das integrantes do grupo feminino de R&B Destiny's Child, que já vendeu mais de 50 milhões de discos mundialmente.",
-        subject: "Artes",
-        cost: "1500.00",
-        weekday: "[0]",
-        time_from: "[750]",
-        time_to: "[1500]"
-    },
-    {
-        name: "Taylor Swift",
-        avatar: "https://avataaars.io/?avatarStyle=Circle&topType=LongHairNotTooLong&accessoriesType=Blank&hairColor=Platinum&facialHairType=Blank&clotheType=GraphicShirt&clotheColor=Pink&graphicType=Resist&eyeType=Wink&eyebrowType=RaisedExcitedNatural&mouthType=Default&skinColor=Pale",
-        whatsapp: "5551988443322",
-        bio: "Taylor Alison Swift é uma cantora e compositora americana. Suas composições narrativas geralmente se concentram em sua vida pessoal e receberam elogios críticos e cobertura da mídia. Nascida em West Reading, Pensilvânia, Swift se mudou para Nashville, Tennessee, em 2004, para seguir uma carreira na música.",
-        subject: "História",
-        cost: "500.00",
-        weekday: "[3]",
-        time_from: "[750]",
-        time_to: "[1500]"
-    }
-];
-
-const subjects = [
-    "Artes",
-    "Biologia",
-    "Ciências",
-    "Educação física",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Português",
-    "Química"
-];
-
-const weekdays = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado"
-];
-
-// FUNCIONALIDADES
-function getSubject(subjectNumber) {
-    const position = +subjectNumber -1; // Expressão retorna número da matéria -1
-    return subjects[position];
-};
-
-function pageLanding(req, res) {
-    return res.render("index.html");
-};
-
-function pageStudy(req, res) {
-    const filters = req.query;
-    return res.render("study.html", {proffys, subjects, weekdays, filters});
-};
-
-function pageGiveClasses(req, res) {
-    const data = req.query;
-    const isEmpty = Object.keys(data).length == 0; // Verifica se está vazio
-
-    // Se há dados
-    if (!isEmpty) {
-        // Altera o número da matéria pelo nome
-        data.subject = getSubject(data.subject);
-
-        // Add na lista de Proffys
-        proffys.push(data);
-
-        return res.redirect("/study");
-    } 
-    
-    // Se não houver dados
-    return res.render("give-classes.html", {subjects, weekdays});
-};
-
 // SERVER
 const express = require('express');
 const server = express();
+const {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses,
+    saveClasses
+} = require('./pages');
 
 const nunjucks = require('nunjucks');   // Config nunjucks
 nunjucks.configure('src/views', {
@@ -92,9 +15,11 @@ nunjucks.configure('src/views', {
 });
 
 server
-.use(express.static("public"))  // Config arquivos estáticos (css, scripts, imagens)
-.get("/", pageLanding)          // Rotas da aplicação
+.use(express.urlencoded({ extended : true}))    // Receber dados req.body
+.use(express.static("public"))                  // Config arquivos estáticos (css, scripts, imagens)
+.get("/", pageLanding)                          // Rotas da aplicação
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+.post("/save-class", saveClasses)
 
 .listen(5500);
